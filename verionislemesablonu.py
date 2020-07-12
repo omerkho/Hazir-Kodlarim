65c06f7227351ebeff4b5766ad74a95271f7162f
omerkho <66906923+omerkho@users.noreply.github.com>
2020-07-12 23:27:01 +0300

    Update verionislemesablonu.py

--- verionislemesablonu.py
+++ verionislemesablonu.py
@@ -1,11 +1,3 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Mar 15 04:18:20 2018
-
-@author: sadievrenseker
-"""
-
 #1. kutuphaneler
 import numpy as np
 import matplotlib.pyplot as plt
@@ -85,9 +77,3 @@ from sklearn.preprocessing import StandardScaler
 sc = StandardScaler()
 X_train = sc.fit_transform(x_train)
 X_test = sc.fit_transform(x_test)
-
-
-
-    
-    
-
