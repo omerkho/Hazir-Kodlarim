b47b243925ed6a5765f248a1a8c7aac2cf2ecaa3
omerkho <66906923+omerkho@users.noreply.github.com>
2020-10-12 18:30:59 +0300

    Create CLUSTERING-COURSERA.py

---
+++ CLUSTERING-COURSERA.py
@@ -0,0 +1,95 @@
+"""
+Created on Fri Sep  4 17:13:04 2020
+
+@author: Omer
+"""
+import random 
+import numpy as np 
+import matplotlib.pyplot as plt 
+from sklearn.cluster import KMeans 
+from sklearn.datasets.samples_generator import make_blobs 
+%matplotlib inline
+np.random.seed(0)
+
+X, y = make_blobs(n_samples=5000, centers=[[4,4], [-2, -1], [2, -3], [1, 1]], cluster_std=0.9)
+plt.scatter(X[:, 0], X[:, 1], marker='.')
+k_means = KMeans(init = "k-means++", n_clusters = 4, n_init = 12)
+k_means.fit(X)
+k_means_labels = k_means.labels_
+k_means_labels
+k_means_cluster_centers = k_means.cluster_centers_
+k_means_cluster_centers
+
+# Initialize the plot with the specified dimensions.
+fig = plt.figure(figsize=(6, 4))
+colors = plt.cm.Spectral(np.linspace(0, 1, len(set(k_means_labels))))
+# Create a plot
+ax = fig.add_subplot(1, 1, 1)
+# For loop that plots the data points and centroids.
+# k will range from 0-3, which will match the possible clusters that each
+# data point is in.
+for k, col in zip(range(len([[4,4], [-2, -1], [2, -3], [1, 1]])), colors):
+    my_members = (k_means_labels == k)
+    cluster_center = k_means_cluster_centers[k]
+    ax.plot(X[my_members, 0], X[my_members, 1], 'w', markerfacecolor=col, marker='.')
+    ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,  markeredgecolor='k', markersize=6)
+
+ax.set_title('KMeans')
+ax.set_xticks(())
+ax.set_yticks(())
+plt.show()
+
+## K = 3 İLE
+k_means3 = KMeans(init = "k-means++", n_clusters = 3, n_init = 12)
+k_means3.fit(X)
+fig = plt.figure(figsize=(6, 4))
+colors = plt.cm.Spectral(np.linspace(0, 1, len(set(k_means3.labels_))))
+ax = fig.add_subplot(1, 1, 1)
+for k, col in zip(range(len(k_means3.cluster_centers_)), colors):
+    my_members = (k_means3.labels_ == k)
+    cluster_center = k_means3.cluster_centers_[k]
+    ax.plot(X[my_members, 0], X[my_members, 1], 'w', markerfacecolor=col, marker='.')
+    ax.plot(cluster_center[0], cluster_center[1], 'o', markerfacecolor=col,  markeredgecolor='k', markersize=6)
+plt.show()
+
+# CUSTOMER SEGMENTATION
+
+import pandas as pd
+cust_df = pd.read_csv("Cust_Segmentation.csv")
+cust_df.head()
+df = cust_df.drop('Address', axis=1)
+df.head()
+from sklearn.preprocessing import StandardScaler
+X = df.values[:,1:]
+X = np.nan_to_num(X)
+Clus_dataSet = StandardScaler().fit_transform(X)
+
+clusterNum = 3
+k_means = KMeans(init = "k-means++", n_clusters = clusterNum, n_init = 12)
+k_means.fit(X)
+labels = k_means.labels_
+print(labels)
+
+df["Clus_km"] = labels
+df.head(5)
+df.groupby('Clus_km').mean()
+
+area = np.pi * ( X[:, 1])**2  
+plt.scatter(X[:, 0], X[:, 3], s=area, c=labels.astype(np.float), alpha=0.5)
+plt.xlabel('Age', fontsize=18)
+plt.ylabel('Income', fontsize=16)
+plt.show()
+
+# 3 boyutlu cizim
+from mpl_toolkits.mplot3d import Axes3D 
+fig = plt.figure(1, figsize=(8, 6))
+plt.clf()
+ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
+plt.cla()
+plt.ylabel('Age', fontsize=18)
+plt.xlabel('Income', fontsize=16)
+#plt.zlabel('Education', fontsize=16)
+ax.set_xlabel('Education')
+ax.set_ylabel('Age')
+#ax.set_zlabel('Income')
+ax.scatter(X[:, 1], X[:, 0], X[:, 3], c= labels.astype(np.float))
